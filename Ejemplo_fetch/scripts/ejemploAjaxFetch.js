f50e70e90d32152c3c6b39ab78846188ac357321
david <davidmc2004@gmail.com>
2025-10-15 13:54:18 +0200

    Ejercicio casi terminado

--- Ejemplo_fetch/scripts/ejemploAjaxFetch.js
+++ Ejemplo_fetch/scripts/ejemploAjaxFetch.js
@@ -50,16 +50,79 @@ function mostrarDatosEmail(usuarios) {
   tbody.innerHTML = "";
 
   usuarios.forEach((usuario) => {
-    const newRow = document.createElement("tr");
+    let newRow = document.createElement("tr");
     newRow.innerHTML = `<td>${usuario.name}</td><td>${usuario.email}</td>`;
+    newRow.innerHTML += `<br>`;
     tbody.appendChild(newRow);
   });
+
+}
+
+function recuperarTarea() {
+  let idTarea = document.getElementById("tareaId").value;
+  if (isNaN(idTarea) || idTarea == "") {
+    alert("Debes introducir un número");
+  } else {
+    fetch("https://jsonplaceholder.typicode.com/todos/" + idTarea)
+      .then((response) => {
+        if (response.status == 200) {
+          return response.json();
+        } else {
+          throw new Error("Error HTTP: " + response.status + " (" + response.statusText + ")");
+        }
+      })
+      .then((tarea) => mostrarTarea(tarea))
+      .catch((error) => {
+        if (error.message.startsWith("Error HTTP")) {
+          console.error(error.message);
+        }
+      });
+  }
+}
+
+function mostrarTarea(tarea) {
+  let tbody = document.getElementsByTagName("tbody")[0];
+  tbody.innerHTML = "";
+  let newRow = document.createElement("tr");
+  newRow.innerHTML = `<td>${tarea.userId}</td><td>${tarea.id}</td><td>${tarea.title}</td><td>${tarea.completed}</td>`;
+  tbody.appendChild(newRow);
 }
 
 function mostrarTareas() {
   fetch("https://jsonplaceholder.typicode.com/todos")
-    .then((response))
+    .then((response) => response.json())
+    .then((tareas) => {
+      let tbody = document.getElementsByTagName("tbody")[0];
+      tbody.innerHTML = "";
+      tareas.forEach((tarea) => {
+        const newRow = document.createElement("tr");
+        newRow.innerHTML = `<td>${tarea.userId}</td><td>${tarea.id}</td><td>${tarea.title}</td><td>${tarea.completed}</td>`;
+        tbody.appendChild(newRow);
+      });
+    })
+    .catch((error) => console.error(error));
 }
 
+function crearTarea() {
+  let nuevaTarea = {
+    userId: 10,
+    title: "Prueba de POST",
+    completed: false
+  };
+
+  let init = {
+    method: "POST",
+    body: JSON.stringify(nuevaTarea),
+    headers: { "Content-type": "application/json;" }
+  };
+
+  fetch("https://jsonplaceholder.typicode.com/todos", init)
+    .then(response => {
+      if (response.status === 201) 
+        return response.json();
+      return "error";
+    })
+    .then(tareaCreada => console.log(tareaCreada))
+}
 
 
